271987cb16144482a807ae8c4a0ec78dc17ae3c8
Daniel Valderrama Arrabal <danivva95@gmail.com>
2022-11-28 12:08:18 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let express = require("express");
 let cors = require("cors");
+let mysql = require ('mysql');
 let app = express();
 
 
+
+/**
+ * Constantes
+ */
 const productoUno = {
     id: 1,
     nombre: "All Might Silver Age",
@@ -60,6 +65,23 @@ const usuarioDos = {
 const usuarios = [usuarioUno, usuarioDos]
 
 
+/**
+ * MySQL
+ */
+let connection = mysql.createConnection({host: "localhost",
+    user: "root",
+    password: "creuhnbvrvbDEGGH7",
+    database: "basedatosproyecto"});
+
+    
+
+
+
+
+/**
+ * Servicios API
+ */
+
 app.use(cors());
 app.use(express.json());
 
@@ -70,6 +92,145 @@ app.get("/", function (request, response) {
 app.get("/productosdestacados", function (request, response) {
     
     response.send(productos);
+});
+
+app.get("/productos/:id", function (request, response) {
+    const productId = request.params.id
+    connection.connect(function(error) {
+        if (error) {
+            console.log(`no es posible conectarse al servidor ${error}`);
+
+            return;
+        }
+        console.log(`Connected to MySQL`);
+    });
+
+    connection.query('select * from productos where productos.id = ?', [productId], function(error, results, fields) {
+        if (error) {
+            console.log(`Se ha producido un error al ejecutar la query: ${error}`);
+
+            return;
+        }
+        console.log(results);
+        response.send(results);
+    }) /*Símbolo de ? es para indicar una variable cuyo valor no sabemos y vamos a indicar entre corchetes. En caso de poner más de uno, hay que poner el valor en orden dentro de los corchetes*/
+
+
+
+    /*connection.end(function(error) {
+        if (error) {
+            console.log(`no se ha podido cerrar la conexión ${error}`);
+
+            return;
+        }
+        console.log(`Conexión cerrada con MySQL`);
+    })*/
+    
+    
+});
+
+
+app.post("/loggin/", function (request, response) {
+    const email = request.body.email;
+    const password = request.body.password;
+    connection.connect(function(error) {
+   
+        if (error) {
+            console.log(`no es posible conectarse al servidor ${error}`);
+
+            return;
+        }
+        console.log(`Connected to MySQL`);
+    });
+
+    connection.query('select * from usuarios where email = ? and password = ? ', [email, password], function(error, results, fields) {
+        if (error) {
+            console.log(`Se ha producido un error al ejecutar la query: ${error}`);
+
+            return;
+        }
+
+        if (results.length > 0) {
+            response.status(200).send();
+        }
+        else if (results.length <= 0){
+            response.status(401).send(); 
+        }
+        console.log(results);
+
+    })
+       
+    });
+
+    app.post("/signup/", function (request, response) {
+        const nombre = request.body.nombre;
+        const apellidos = request.body.apellidos;
+        const email = request.body.email;
+        const password = request.body.password;
+        const telefono = request.body.telefono;
+        connection.connect(function(error) {
+       
+            if (error) {
+                console.log(`no es posible conectarse al servidor ${error}`);
+    
+                return;
+            }
+            console.log(`Connected to MySQL`);
+        });
+    
+        connection.query('insert into usuarios (nombre, apellidos, email, password, telefono) values (?, ?, ?, ?, ?) ', 
+        [nombre,apellidos, email, password, telefono], function(error, results, fields) {
+            if (error) {
+                console.log(`Se ha producido un error al ejecutar la query: ${error}`);
+    
+                response.status(400).send();
+            }
+
+            connection.query("select * from usuarios where email = ?", [email], function(error, results, fields) {
+                if (error) {
+                    console.log(`se ha producido un error al ejecutar la query: ${error}`);
+                    response.status(400).send();
+                }
+            });
+
+            response.send(results)
+            console.log(results);
+           
+        }) 
+
+
+
+    
+    
+    
+});
+
+app.post("/orders", function (request, response) {
+
+    let tarjetaId = request.body.tarjeta
+    
+    connection.connect(function(error) {
+   
+        if (error) {
+            console.log(`no es posible conectarse al servidor ${error}`);
+
+            return;
+        }
+        console.log(`Connected to MySQL`);
+    });
+
+    connection.query('insert into pedidos (usuarioid, productoid, direccionid, tarjetaid, estado, total, tipoPago) values (?, ?, ?, ?, ?, ?, ?) ', 
+    ["1","2", "calle arenisca", tarjetaId, "not shipped", "50€", ], function(error, results, fields) {
+        if (error) {
+            console.log(`Se ha producido un error al ejecutar la query: ${error}`);
+
+            response.status(400).send();
+        }
+
+        response.send(results)
+        console.log(results);
+       
+    }) 
 });
 
 app.get("/detalles", function (request, response) {
@@ -110,8 +271,8 @@ app.post("/login", function (request, response) {
 
 app.post("/signin", function (request, response){
 
-})
+});
 
 app.listen(8000, function() {
     console.log("API ready to listen");
-});
+})
